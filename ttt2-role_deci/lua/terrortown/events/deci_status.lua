29e9687c02a0d02cfbe53a78d280a598695f0dd8
milkwxter <ethan.r.bates2003@gmail.com>
2024-05-31 11:50:53 -0500

    Updated README and moved more strings to lang file

--- ttt2-role_deci/lua/terrortown/events/deci_status.lua
+++ ttt2-role_deci/lua/terrortown/events/deci_status.lua
@@ -1,33 +1,37 @@
 if SERVER then
+	-- Add icons to clients machines
 	resource.AddFile("materials/vgui/ttt/icons/charging.png")
+	resource.AddFile("materials/vgui/ttt/icons/timer.png")
+
+	-- Getting ready to accept this string
 	util.AddNetworkString("ttt2_deci_hit_ply")
-end
--- 
-if SERVER then
+
+	-- When this string is broadcasted, we write important info that only the server has
 	hook.Add("TTTGetDeciPly","ttt_get_deci_ply",function(arg)
 		net.Start("ttt2_deci_hit_ply")
 		net.WriteString(arg:GetRoleString())
 		net.WriteColor(arg:GetRoleColor())
-		--LANG.MsgAll(arg:GetRoleString(), nil, MSG_MSTACK_WARN)
 		net.Broadcast()
 	end)
 end
+
+-- These are the status conditions found in the role
 if CLIENT then
 	hook.Add("Initialize", "ttt2_deci_init", function()
 		-- Decipherer's Charging Cooldown
 		STATUS:RegisterStatus("ttt2_deci_cooldown_stat", {
 			hud = Material("vgui/ttt/icons/charging.png"),
 			type = "bad",
-			name = "Minitester charging cooldown",
-			sidebarDescription = "You cannot decipher roles until your minitester is charged again."
+			name = "lang_deci_status_charging",
+			sidebarDescription = "lang_deci_status_charging_desc"
 		})
 
 		-- Decipherer's Minitester Result Timer
 		STATUS:RegisterStatus("ttt2_deci_results_timer_stat", {
 			hud = Material("vgui/ttt/icons/timer.png"),
 			type = "good",
-			name = "Minitester results incoming",
-			sidebarDescription = "When this timer hits 0, you will find out the player's role."
+			name = "lang_deci_status_timer",
+			sidebarDescription = "lang_deci_status_timer_desc"
 		})
 	end)
 end
--- ttt2-role_deci/lua/terrortown/lang/en/decipherer.lua
+++ ttt2-role_deci/lua/terrortown/lang/en/decipherer.lua
@@ -8,3 +8,11 @@ L["search_role_" .. DECIPHERER.abbr] = "This person was a Decipherer!"
 L["target_" .. DECIPHERER.name] = "Decipherer"
 L["ttt2_desc_" .. DECIPHERER.name] = [[The Decipherer needs to use his Decipering Tool to learn the team of fellow terrorists.]]
 L["credit_" .. DECIPHERER.abbr .. "_all"] = "Decipherer, you have been awarded {num} equipment credit(s) for your performance."
+
+-- STATUS LANGUAGE STRINGS
+L["lang_deci_status_charging"] = "Minitester charging cooldown"
+L["lang_deci_status_charging_desc"] = "You cannot decipher roles until your minitester is charged again."
+L["lang_deci_status_timer"] = "Minitester results incoming"
+L["lang_deci_status_timer_desc"] = "When this timer hits 0, you will find out the player's role."
+
+-- WEAPON LANGUAGE STRINGS
